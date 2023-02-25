cd07bd6579dde80a139e6fc3969bbd062dcdbba8
Kristian <ivanov_15@mail.bg>
2023-02-25 17:30:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+?>
+<!DOCTYPE html>
+    <head>	
+        <title>Home</title>
+    </head>
+    <body>    
+        <table id="tempatures"></table>
+
+        <script type="text/javascript" src="script/script.js"></script> 
+    </body>
+</html>
---
+++ script/script.js
@@ -0,0 +1,25 @@
+async function getTempatures() {
+	let url = 'data/data.json';
+	try {
+			let result = await fetch(url);
+			return await result.json();
+	} catch (error) {
+			console.log(error);
+	}
+}
+
+async function renderData() {
+	let tempatures = await getTempatures();
+
+	let li = `<tr><th>Location Name</th></tr>`;
+
+	Object.entries(tempatures).forEach((regulation) => {
+		  li += `<tr>
+		    <td>${regulation[0]} </td>
+		  </tr>`;
+		});
+
+		document.getElementById("tempatures").innerHTML = li;
+}
+
+renderData();
